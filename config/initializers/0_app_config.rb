c7aff4d1e9aabd01e4f9bbcc288dccbdebeeb3f7
Alexander Stefanov-Khryukin <nobodydead@gmail.com>
2015-11-09 14:03:24 +0000

    replace APP_CONFIG with proper one

--- config/initializers/0_app_config.rb
+++ config/initializers/0_app_config.rb
@@ -1,4 +1,4 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]
+APP_CONFIG = Rails.application.config_for('application')
 # Remove '/' from the end of url
 APP_CONFIG.keys.select {|key| key =~ /_url\Z/}.each {|key| APP_CONFIG[key] = APP_CONFIG[key].chomp('/') if APP_CONFIG[key].respond_to?(:chomp)}
 # Paginates a static array
