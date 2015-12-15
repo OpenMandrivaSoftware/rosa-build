4cccb94af13a949171992158e9bf383a1ee8bcc7
duratarskeyk <shadowpilot34@gmail.com>
2015-12-15 13:13:52 +0000

    Removed .presence

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
 class Redis
   def self.connect!
-    puts ENV["REDIS_URL"]
-    puts ENV["REDIS_URL"].presence
-    url  = ENV["REDIS_URL"].presence || "redis://localhost:6379/#{::Rails.env.test? ? 1 : 0}"
+    url  = ENV["REDIS_URL"] || "redis://localhost:6379/#{::Rails.env.test? ? 1 : 0}"
     opts = { url: url }
 
     opts[:logger] = ::Rails.logger if ::Rails.application.config.log_redis
