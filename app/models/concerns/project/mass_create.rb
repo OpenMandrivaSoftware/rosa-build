9b4f207fe3bdb4351c4c1d48ac8bbd3ae3387ccf
duratarskeyk <shadowpilot34@gmail.com>
2015-12-18 00:42:55 +0000

    Using transactions to create projects

--- app/models/concerns/project/mass_create.rb
+++ app/models/concerns/project/mass_create.rb
@@ -7,21 +7,26 @@ module Project::MassCreate
   module ClassMethods
     def run_mass_create(url, visibility, owner, add_to_repository_id)
       repository = Repository.find add_to_repository_id
-      open(url) { |f|
-        f.each_line {
-            |line|
+      open(url) do |f|
+        projects = []
+        ActiveRecord::Base.transaction do
+          f.each_line do |line|
             project = owner.projects.build(
               name: line,
               visibility:  visibility,
               is_package: false
             )
             project.owner = owner
-            if project.save
-              repository.projects << project rescue nil
-              project.update_attributes(is_package: true)
-            end
-          }
-        }
+            projects << project if project.save
+          end
+        end
+        ActiveRecord::Base.transaction do
+          projects.each do |item|
+            repository.projects << item
+            item.update_attributes(is_package: true)
+          end
+        end
+      end
     end
   end
 end
