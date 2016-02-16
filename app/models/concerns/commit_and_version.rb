a82b6adc1487518b97f7d08ac8e7c6855dc2253a
Wedge <shadowpilot34@gmail.com>
2016-02-16 18:37:25 +0300

    Returned setting project version to commit hash if project version is not present

--- app/models/concerns/commit_and_version.rb
+++ app/models/concerns/commit_and_version.rb
@@ -2,15 +2,22 @@ module CommitAndVersion
   extend ActiveSupport::Concern
 
   included do
+    before_validation :set_version
     before_create :set_last_published_commit
   end
 
   protected
 
+  def set_version
+    if project_version.blank? && commit_hash.present?
+      self.project_version = commit_hash
+    end
+  end
+
   def set_last_published_commit
     return unless self.respond_to? :last_published_commit_hash # product?
     last_commit = self.last_published.first.try :commit_hash
-    if last_commit && self.project.repo.commit(last_commit).present? # commit(nil) is not nil!
+    if last_commit # commit(nil) is not nil!
       self.last_published_commit_hash = last_commit
     end
   end
