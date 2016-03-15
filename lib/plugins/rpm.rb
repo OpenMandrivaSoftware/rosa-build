b8841e78c77f5f39ac4bace70ee892a15c2e62e3
Alexander Khryukin <alexander@mezon.ru>
2016-03-15 19:10:13 +0300

    add librpm-5.4.so to rpm.rb

--- lib/plugins/rpm.rb
+++ lib/plugins/rpm.rb
@@ -6,7 +6,7 @@ module RPM
     extend ::FFI::Library
 
     begin
-      ffi_lib ['librpm.so.3', 'librpm.so.2', 'librpm.so.1', 'rpm']
+      ffi_lib ['librpm.so.3', 'librpm.so.2', 'librpm.so.1', 'librpm-5.4.so', 'rpm']
     rescue LoadError => e
       raise(
         "Can't find rpm libs on your system: #{e.message}"
