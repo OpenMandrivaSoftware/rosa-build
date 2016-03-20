db07de8d7b503fb9c8ea7e75581eb4779524e2f5
Wedge <shadowpilot34@gmail.com>
2016-03-20 15:27:06 +0300

    Get rid of codemirror

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   login.css login.js reg_session.css tour.css tour.js gollum/editor/langs/*.js
-  moment/ru.js codemirror_editor.js codemirror_editor.css new_application.css
-  new_application.js angular-locale_ru-ru.js
+  moment/ru.js new_application.css new_application.js angular-locale_ru-ru.js
 )
