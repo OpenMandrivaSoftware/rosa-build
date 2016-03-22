d5180f73d74ffcc59963b79e72b2496ecc3ecbcc
Wedge <shadowpilot34@gmail.com>
2016-03-22 20:02:49 +0300

    Code clean up, removed access to api via standart login

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::BaseController < ApplicationController
   include PaginateHelper
   respond_to :json
+  skip_before_action :verify_authenticity_token
+  before_action :check_auth
 
   helper_method :member_path
 
@@ -12,6 +14,14 @@ class Api::V1::BaseController < ApplicationController
   end
 
   protected
+
+  def check_auth
+    authenticate_or_request_with_http_basic do |token,pw|
+      if user = User.find_by_authentication_token(token)
+        sign_in user, false
+      end
+    end
+  end
 
   def set_csv_file_headers(file_name)
     headers['Content-Type'] = 'text/csv'
--- app/controllers/projects/base_controller.rb
+++ app/controllers/projects/base_controller.rb
@@ -1,17 +1,9 @@
 class Projects::BaseController < ApplicationController
-  prepend_before_action :authenticate_user_and_find_project
+  prepend_before_action :find_project
 
   protected
 
-  def find_collaborators
-    search = "%#{params[:search_user]}%"
-    @users = @project.collaborators.where("users.uname ILIKE ?", search)
-    @users |= @project.owner.members.where("users.uname ILIKE ?", search) if @project.owner.is_a?(Group)
-    @users = @users.sort_by(&:uname).first(10)
-  end
-
-  def authenticate_user_and_find_project
-    authenticate_user
+  def find_project
     return if params[:name_with_owner].blank?
     authorize @project = Project.find_by_owner_and_name!(params[:name_with_owner]), :show?
   end
