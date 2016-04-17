7571352be13ffdf6d7af9685199fdb92808bd603
DuratarskeyK <shadowpilot34@gmail.com>
2016-04-17 15:20:42 +0400

    change regexp that finds cached chroot archives

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -269,7 +269,7 @@ class Platform < ActiveRecord::Base
       next false unless product
       pbl = product.product_build_lists.for_status(ProductBuildList::BUILD_COMPLETED).recent.first
       next false unless pbl
-      result = pbl.results.find{ |r| r['file_name'] =~ /-#{arch}.tar.gz$/ }
+      result = pbl.results.find{ |r| r['file_name'] =~ /-#{arch}.tar.(gz|xz)$/ }
       result.present? ? result['sha1'] : false
     end
   end
