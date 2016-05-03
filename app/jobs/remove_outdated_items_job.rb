8940501d307f0e41bd74cd96335649f60fcace5b
DuratarskeyK <shadowpilot34@gmail.com>
2016-05-03 22:04:24 +0400

    remove activity feeds too

--- app/jobs/remove_outdated_items_job.rb
+++ app/jobs/remove_outdated_items_job.rb
@@ -13,6 +13,9 @@ class RemoveOutdatedItemsJob
     end
     counter_pbl = ProductBuildList.outdated.count
     ProductBuildList.outdated.destroy_all
+    User.find_each(batch_size: 50) do |u|
+      u.activity_feeds.outdated.destroy_all
+    end
     File.open(log_file, "w") do |f|
       f.puts "Build Lists deleted: #{counter_bl}"
       f.puts "Mass Builds deleted: #{counter_mb}"
