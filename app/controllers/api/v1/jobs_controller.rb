198def949b2f8b219f7ca733fc09b658ae110e7c
Wedge <shadowpilot34@gmail.com>
2016-06-10 21:56:03 +0300

    Typo fix

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -104,7 +104,7 @@ class Api::V1::JobsController < Api::V1::BaseController
   def clear_stale_builders
     BuildList.transaction do
       BuildList.where(["updated_at < ?", 120.seconds.ago]).where(status: BuildList::BUILD_PENDING).where.not(builder: nil).find_each(batch_size: 50) do |bl|
-        bl.update_column(builder_id: nil)
+        bl.update_column(:builder_id, nil)
       end
     end
   end
