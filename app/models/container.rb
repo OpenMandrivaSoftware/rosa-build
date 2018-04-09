31f68329b36a6dc4e551f200234e8cdac0cfdf99
Wedge <shadowpilot34@gmail.com>
2018-04-09 14:27:32 +0300

    Use sentry for error processing

--- app/models/container.rb
+++
@@ -1,4 +0,0 @@
-class Container < ActiveRecord::Base
-  belongs_to :platform
-  has_many :build_lists
-end
--- db/migrate/20170129103216_add_container_to_build_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddContainerToBuildLists < ActiveRecord::Migration
-  def change
-    add_reference :build_lists, :container, index: true, foreign_key: true
-  end
-end
