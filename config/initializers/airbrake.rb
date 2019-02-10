0c4e1e6f919c9b919450d6127dd7c4e69aa4b792
Wedge <shadowpilot34@gmail.com>
2019-02-10 17:04:07 +0300

    remove airbrake

--- config/initializers/airbrake.rb
+++
@@ -1,8 +0,0 @@
-require 'airbrake'
-
-#Airbrake.configure do |config|
-#  config.project_key = APP_CONFIG['airbrake']['api_key']
-#  config.host    = APP_CONFIG['airbrake']['host']
-  #config.port    = 80
-  #config.secure  = config.port == 443
-#end
