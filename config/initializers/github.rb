1a8691ad2feee931546f9b7a6a319263c1ac70e3
Wedge <shadowpilot34@gmail.com>
2020-11-15 03:07:03 +0300

    Use access tokens

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,10 +1,9 @@
 #configuration of Github api gem
 
-Github_blobs_api = Octokit::Client.new(login: ENV['GITHUB_REPO_BOT_LOGIN'], password: ENV['GITHUB_REPO_BOT_PASSWORD'])
+Github_blobs_api = Octokit::Client.new(access_token: ENV["GITHUB_REPO_BOT_ACCESS_TOKEN"])
 
 Octokit.configure do |c|
-  c.login = ENV["GITHUB_LOGIN"]
-  c.password = ENV["GITHUB_PASSWORD"]
+  c.access_token = ENV["GITHUB_ACCESS_TOKEN"]
 end
 Octokit.middleware = Faraday::RackBuilder.new do |builder|
   store = ActiveSupport::Cache.lookup_store(:redis_store, ENV['REDIS_URL'].to_s + '/1')
